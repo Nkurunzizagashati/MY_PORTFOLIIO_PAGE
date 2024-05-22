d3172b6256a1e9b925123866c784858c1e9b14f1
Fabrice <f.nkurunziz@alustudent.com>
2024-05-22 14:54:34 +0200

    now added the js for showing and hidding the navList when the hamMenu is clicked

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ let typed = new Typed("#typed", {
   typeSpeed: 50,
   loop: true,
 });
+
+const hamMenu = document.querySelector(".hamMenu");
+
+hamMenu.addEventListener("click", () => {
+  const navContainer = document.querySelector(".nav_container");
+  const navListOpen = navContainer.getAttribute("navListOpen");
+
+  if (navListOpen === "false") {
+    document
+      .querySelector(".nav_container")
+      .setAttribute("navListOpen", "true");
+  } else {
+    document
+      .querySelector(".nav_container")
+      .setAttribute("navListOpen", "false");
+  }
+});
